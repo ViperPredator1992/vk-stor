882bfcbe0ba95f7790ba251ddf5d8c359ac92bf6
AlexanderSemenov92 <42918575+AlexanderSemenov92@users.noreply.github.com>
2019-10-23 00:35:31 +0300

    add popup

--- js/script.js
+++ js/script.js
@@ -10,5 +10,17 @@ $(document).ready(function () {
             $(this).next(".nav").slideUp();
         }
     });
+
+    $(".table tbody tr td a").click(function(e){
+        e.preventDefault();
+        $('.modal').addClass('modal-active');
+        $('.mask').addClass('mask-active');
+    });
+
+    $(".mask, .modal-close__icon").click(function(e){
+        e.preventDefault();
+        $('.modal').removeClass('modal-active');
+        $('.mask').removeClass('mask-active');
+    });
     
 });
