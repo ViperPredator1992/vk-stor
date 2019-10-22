556fcdf81aac54247667020e7ef0314be3630191
AlexanderSemenov92 <42918575+AlexanderSemenov92@users.noreply.github.com>
2019-10-22 18:26:06 +0300

    add header

--- js/script.js
+++ js/script.js
@@ -1,5 +1,14 @@
 $(document).ready(function () {
    
-  
+    $(".burger").click(function(){
+        if($(this).next(".nav").css("display")=="none") {
+            $(this).next(".nav").slideDown();
+            $(".burger").addClass("burger__open");
+        }
+        else {
+            $(".burger").removeClass("burger__open");
+            $(this).next(".nav").slideUp();
+        }
+    });
     
 });
